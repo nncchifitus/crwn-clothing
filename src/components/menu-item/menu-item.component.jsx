30151c91d41dd47aedc898149302a43aad7a6788
Nguyen Ngoc Chung Chi <nncchi.fitus@gmail..com>
2021-08-10 13:56:13 +0700

    updating with a shop page, collection preview and collection item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,10 +3,13 @@ import { withRouter } from "react-router-dom";
 
 import "./menu-item.style.scss";
 
-const MenuItem = ({ title, imageUrl, size, history, linkUrl, match, location }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
   <div
     className={`${size} menu-item`}
-    onClick={() => history.push(`${match.url}${linkUrl}`)}
+    onClick={() => {
+      console.log(match.url);
+      history.push(`${match.url}${linkUrl}`);
+    }}
   >
     <div
       style={{
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
+import ShopPage from "../shop/shop.component";
+
 import Directory from "../../components/directory/directory.component";
 
 import "./homepage.style.scss";
 
 const HomePage = (props) => (
   <div className="homepage">
-    {console.log(props.history)}
-    {console.log(props.match)}
-    {console.log(props.location)}
     <Directory />
+    <ShopPage />
   </div>
 );
 
