d94002ba9c419328e3a37e69d697f71e19778d82
Nguyen Ngoc Chung Chi <nncchi.fitus@gmail..com>
2021-08-10 15:28:46 +0700

    remove shoppage from homepage

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import ShopPage from "../shop/shop.component";
-
 import Directory from "../../components/directory/directory.component";
 
 import "./homepage.style.scss";
@@ -9,7 +7,6 @@ import "./homepage.style.scss";
 const HomePage = (props) => (
   <div className="homepage">
     <Directory />
-    <ShopPage />
   </div>
 );
 
