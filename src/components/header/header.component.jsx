693aa6a052489c1c1718dc49f8a79fbe1a27b03b
Nguyen Ngoc Chung Chi <nncchi.fitus@gmail..com>
2021-08-10 14:33:20 +0700

    adding header component and links to other pages

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+
+import "./header.style.scss";
+
+const Header = () => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link className="option" to="/">
+        HOME
+      </Link>
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
+      <Link className="option" to="/contact">
+        CONTACT
+      </Link>
+      <Link className="option" to="/sign-in">
+        SIGN IN
+      </Link>
+      <Link className="cart-container" to="/cart">
+        CART
+      </Link>
+    </div>
+  </div>
+);
+
+export default Header;
