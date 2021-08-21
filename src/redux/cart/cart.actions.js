3d920ec3d3286fd595624f4d2d885891f488e026
Nguyen Ngoc Chung Chi <nncchi.fitus@gmail..com>
2021-08-21 17:45:45 +0700

    added addItem and removeItem functionality to checkout item component

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -10,3 +10,27 @@ export const addItem = (item) => ({
   // Trong trường hợp này nó là item chúng ta muốn thêm vào cartItems array
   payload: item,
 });
+
+export const removeItem = (item) => ({
+  type: CartActionTypes.REMOVE_ITEM,
+  payload: item,
+  // Payload là item chúng ta muốn loại khỏi cartItems array
+});
+
+export const clearItemFromCart = (item) => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item,
+  // Payload là item chúng ta muốn loại khỏi cartItems array
+});
+
+export const increaseQuantityItem = (item) => ({
+  type: CartActionTypes.INCREASE_QUANTITY_ITEM,
+  payload: item,
+  // Payload là item chúng ta muốn tăng số lượng cartItems array
+});
+
+export const decreaseQuantityItem = (item) => ({
+  type: CartActionTypes.DECREASE_QUANTITY_ITEM,
+  payload: item,
+  // Payload là item chúng ta muốn giảm số lượng cartItems array
+});
